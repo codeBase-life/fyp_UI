3a884c80aadb97acd7be493f1e25e187dc409592
malik rasheed <158214001+codeBase-life@users.noreply.github.com>
2025-03-17 17:19:51 +0500

    test component causing a error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import UserForm from "./components/UserForm";
 import Dashboard from "./components/Dashboard";
 import Authentication from "./UserAuth/Authentication";
 import { AppProvider } from "./Context/AppContext";
-import Test from "./components/test";
+
 import "./App.css";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
           </Route>
 
-          <Route path="/test" element={<Test />} />
+          
 
           {/* protected route - now uses context instead of props */}
         </Routes>
